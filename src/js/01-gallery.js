84f54992f2ac20cf7dc16681f0129afb12c9227c
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-05-26 00:01:17 +0200

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
 
 console.log(galleryItems);
+const gallery = document.body.querySelector(".gallery");
+const markup = galleryItems.map((image) => `<a class="gallery__item" href="${image.original}">
+                                            <img class="gallery__image" src=${image.preview} alt=${image.description}
+                                            </a>`).join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData:"alt", captionDelay:250 });
