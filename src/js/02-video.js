084005840866552d658aef522874f2b3274afa2e
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-05-26 15:10:47 +0200

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    console.log('played the video!');
+};
+
+player.on('timeupdate', onPlay);
 
