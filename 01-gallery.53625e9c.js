e0e6b344a401f6c3d18d2a4c877528882ab21f41
StanislawZielinski <94519797+StanislawZielinski@users.noreply.github.com>
2022-05-26 08:37:04 +0000

    Deploying to gh-pages from @ StanislawZielinski/goit-js-hw-08@c35b233b7e457bbb887a3ff364f681922265d4ff 🚀

--- 01-gallery.53625e9c.js
+++ 01-gallery.53625e9c.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=require("./gallery-items"),l=a(require("simplelightbox"));function a(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css"),console.log(e.galleryItems);const i=document.body.querySelector(".gallery"),t=e.galleryItems.map(e=>`<a class="gallery__item" href="${e.original}">\n                                            <img class="gallery__image" src=${e.preview} alt=${e.description}\n                                            </a>`).join("");i.insertAdjacentHTML("afterbegin",t);let r=new l.default(".gallery a",{captionsData:"alt",captionDelay:250});
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel-project-template/01-gallery.53625e9c.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.53625e9c.js.map
