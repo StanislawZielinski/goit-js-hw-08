97f02ff37166d9b507ab48d42ac78f7420541a84
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-05-28 15:44:15 +0200

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,28 +10,13 @@ player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
 
-const test = _.throttle(function() {console.log("Function throttled after 1000ms!");}, 4000);
- 
-const onPlay = function(data) {localStorage.setItem("videoplayer-current-time", JSON.stringify(data))};
+const testTime = _.throttle((data) => { localStorage.setItem("videoplayer-current-time", JSON.stringify(data)); }, 1000);
+const onPlay = function(data) {
+    testTime(data);
+};
 
-    // test();
 
-
-// player.on(_.throttle(function () { 'timeupdate' }, 5000), onPlay);
 player.on('timeupdate',onPlay);
 const savedTime = localStorage.getItem("videoplayer-current-time")
 const currentTime = JSON.parse(savedTime).seconds;
 player.setCurrentTime(currentTime);
-
-
-// var throt_fun = _.throttle(function() {
-//         console.log('Function throttled after 1000ms!');
-// }, 4000);
-//     // Defining loop
-// var loop = function() {
-//     setTimeout(loop, 1000);
-//     throt_fun();
-// };
-  
-// // Calling loop to start
-// loop();
