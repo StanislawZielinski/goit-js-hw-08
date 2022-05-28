594f21194b59ef2739af07bfea43636e91249c08
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-05-28 11:48:53 +0200

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
+const _= require('lodash');
 player.on('play', function() {
     console.log('played the video!');
 });
@@ -10,9 +10,28 @@ player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
 
-const onPlay = function(data) {
-    console.log('played the video!');
-};
+const test = _.throttle(function() {console.log("Function throttled after 1000ms!");}, 4000);
+ 
+const onPlay = function(data) {localStorage.setItem("videoplayer-current-time", JSON.stringify(data))};
 
-player.on('timeupdate', onPlay);
+    // test();
 
+
+// player.on(_.throttle(function () { 'timeupdate' }, 5000), onPlay);
+player.on('timeupdate',onPlay);
+const savedTime = localStorage.getItem("videoplayer-current-time")
+const currentTime = JSON.parse(savedTime).seconds;
+player.setCurrentTime(currentTime);
+
+
+// var throt_fun = _.throttle(function() {
+//         console.log('Function throttled after 1000ms!');
+// }, 4000);
+//     // Defining loop
+// var loop = function() {
+//     setTimeout(loop, 1000);
+//     throt_fun();
+// };
+  
+// // Calling loop to start
+// loop();
